116082898cf34c7c105708b587b3f5f28ae5a3cf
Yang Liu <jeremyrobturtle@gmail.com>
2018-09-26 11:14:30 -0700

    do not update positions if scope changed

--- lib/acts_as_list/active_record/acts/callback_definer.rb
+++ lib/acts_as_list/active_record/acts/callback_definer.rb
@@ -8,7 +8,6 @@ module ActiveRecord::Acts::List::CallbackDefiner #:nodoc:
       before_destroy :reload, unless: Proc.new { new_record? || destroyed_via_scope? || act_as_list_no_update? }
       after_destroy :decrement_positions_on_lower_items, unless: Proc.new { destroyed_via_scope? || act_as_list_no_update? }
 
-      before_update :check_scope, unless: :act_as_list_no_update?
       after_update :update_positions, unless: :act_as_list_no_update?
 
       after_commit :clear_scope_changed
