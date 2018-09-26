e1fad26cd9db267e6e71a6e20fbcc5857b35e980
Yang Liu <jeremyrobturtle@gmail.com>
2018-09-26 11:41:35 -0700

    do not update positions in callbacks

--- lib/acts_as_list/active_record/acts/callback_definer.rb
+++ lib/acts_as_list/active_record/acts/callback_definer.rb
@@ -8,8 +8,6 @@ module ActiveRecord::Acts::List::CallbackDefiner #:nodoc:
       before_destroy :reload, unless: Proc.new { new_record? || destroyed_via_scope? || act_as_list_no_update? }
       after_destroy :decrement_positions_on_lower_items, unless: Proc.new { destroyed_via_scope? || act_as_list_no_update? }
 
-      after_update :update_positions, unless: :act_as_list_no_update?
-
       after_commit :clear_scope_changed
 
       if add_new_at.present?
