613e2e73d5799c874f3617d3bce739c9ff7bfd4b
Bretton Auerbach <bretton.auerbach@gmail.com>
2018-09-26 17:23:27 -0700

    dont resort the list when adding a new record

--- lib/acts_as_list/active_record/acts/callback_definer.rb
+++ lib/acts_as_list/active_record/acts/callback_definer.rb
@@ -9,10 +9,6 @@ module ActiveRecord::Acts::List::CallbackDefiner #:nodoc:
       after_destroy :decrement_positions_on_lower_items, unless: Proc.new { destroyed_via_scope? || act_as_list_no_update? }
 
       after_commit :clear_scope_changed
-
-      if add_new_at.present?
-        before_create "add_to_list_#{add_new_at}".to_sym, unless: :act_as_list_no_update?
-      end
     end
   end
 end
